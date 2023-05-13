25fd9eeccb89b06a8ab8f76873960422bb79de43
Petr <vinokurovvin@list.ru>
2023-05-13 14:17:01 +0900

    pnpm

--- npmworkspace/examples/vite-vue3/vite.config.ts
+++ npmworkspace/examples/vite-vue3/vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5023
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    }
   }
 })
