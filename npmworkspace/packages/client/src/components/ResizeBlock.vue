cb7bf07f11232eabbb72db66410708e4c30693bd
Petr <vinokurovvin@list.ru>
2023-05-14 22:12:03 +0900

    useElementSize

--- npmworkspace/packages/client/src/components/ResizeBlock.vue
+++ npmworkspace/packages/client/src/components/ResizeBlock.vue
@@ -1,57 +1,55 @@
 <template>
-  <div class="resize-block">
-    <div class="resize-block__top-info">
-        w:{{ resizeHelper.data.info.width }} h: {{ resizeHelper.data.info.height }}
-    </div>
-    <div ref="el" class="resize-block__bottom-rigth-resizer">WH</div>
-    <div
-      class="resize-block__block"
-      :style="{
-        width: `${resizeHelper.data.info.width}px`,
-        height: `${resizeHelper.data.info.height}px`,
-      }"
-    >
+  <div >
+    h:{{ height }} /  w:{{ width }}
+  </div>
+  <div :class="$style.resizable">
+    <div ref="window" style="height: 100%; width: 100%; border: 1px solid black;">
       <slot></slot>
     </div>
+    
   </div>
 </template>
 <script lang="ts" setup>
-import useResizeHelper from "@/composed/resizeHelper";
-import { onMounted, ref } from "vue";
 
-const resizeHelper = useResizeHelper();
+import { onMounted, ref, useCssModule } from "vue";
+import interact from "interactjs"
+import { useElementSize } from '@vueuse/core'
 
-const el = ref<HTMLDivElement | null>(null);
+const window = ref<HTMLDivElement | null>(null)
+
+const cssModule = useCssModule()
+const {width, height} = useElementSize(window)
+
 onMounted(() => {
-  resizeHelper.set(el.value!, {
-    initialHeight: 500,
-    initialWidth: 500,
-  });
+  interact(`.${cssModule.resizable}`)
+  .resizable({
+    edges: { top: false, left: false, bottom: true, right: true },
+    listeners: {
+      move: function (event) {
+        let { x, y } = event.target.dataset
+
+        x = (parseFloat(x) || 0) + event.deltaRect.left
+        y = (parseFloat(y) || 0) + event.deltaRect.top
+
+        Object.assign(event.target.style, {
+          width: `${event.rect.width}px`,
+          height: `${event.rect.height}px`,
+          transform: `translate(${x}px, ${y}px)`
+        })
+
+        Object.assign(event.target.dataset, { x, y })
+      }
+    }
+  })
 });
 </script>
-<style scoped>
-.resize-block {
-  display: inline-block;
-  padding: 0 40px 40px 40px;
+<style module>
+.resizable {
+  width: 500px;
+  height: 500px;
+  border-radius: 0.75rem;
+  padding: 50px;
   background-color: rgb(255, 255, 255);
-  border-radius: 50px;
-  position: relative;
-}
-.resize-block__block {
-  background-color: rgb(128, 136, 136);
-  border: 1px rgb(192, 187, 187) solid;
-}
-.resize-block__bottom-rigth-resizer {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  background-color: beige;
-  cursor: nw-resize;
-}
-.resize-block__top-info {
-    height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  box-sizing: border-box;
 }
 </style>
