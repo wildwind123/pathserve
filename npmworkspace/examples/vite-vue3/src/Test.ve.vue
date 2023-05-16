30bae2122b5582d3ec0a6a6695c90826c686161b
Petr <vinokurovvin@list.ru>
2023-05-16 22:53:47 +0900

    sendInput1

--- npmworkspace/examples/vite-vue3/src/Test.ve.vue
+++ npmworkspace/examples/vite-vue3/src/Test.ve.vue
@@ -2,26 +2,26 @@
   <hr>
   <p >input 1</p>
   <p>
-    <button @click="setInput1('button')">change input 1 to 'button'</button>
+    <button @click="sendInput1('button')">change input 1 to 'button'</button>
   </p>
   <input
     type="text"
     :value="testValue"
-    @input="setInput1(($event.target as HTMLInputElement).value)"
+    @input="sendInput1(($event.target as HTMLInputElement).value)"
   />
   <hr>
   <p>input 2</p>
   <input
     type="text"
     :value="testValue2"
-    @input="setInput2(($event.target as HTMLInputElement).value)"
+    @input="sendInput2(($event.target as HTMLInputElement).value)"
   />
   <hr>
   <p>input 3</p>
   <input
     type="number"
     :value="testNumberValue"
-    @input="setInput3(parseInt(($event.target as HTMLInputElement).value))"
+    @input="sendInput3(parseInt(($event.target as HTMLInputElement).value))"
   />
   <hr>
   <label>input 4</label>
@@ -37,7 +37,7 @@
           id: testObject.id + 1,
           value: testObject.id + testObject.value,
         };
-        setInput4(testObject);
+        sendInput4(testObject);
       }
     "
   >
@@ -67,26 +67,26 @@ interface testObject {
 }
 // input 1
 const testValue = ref("input 1");
-const [setInput1] = useString("input 1",testValue.value, (value) => {
+const [sendInput1] = useString("input 1",testValue.value, (value) => {
   testValue.value = value;
 });
 //  input 2
 const testValue2 = ref(" input 2");
-const [setInput2] = useString(
+const [sendInput2] = useString(
   "input 2",
   testValue2.value,
   (value) => (testValue2.value = value)
 );
 // input 3
 const testNumberValue = ref(0);
-const [ setInput3 ] = useNumber(
+const [ sendInput3 ] = useNumber(
   "input 3",
   testNumberValue.value,
   (value) => (testNumberValue.value = value)
 );
 //  input 4
 const testObject = ref<testObject>({ id: 1, value: "input 4" });
-const [setInput4] = useObject(
+const [sendInput4] = useObject(
   "input 4",
   toRaw(testObject.value),
   (value) => (testObject.value = value as testObject)
